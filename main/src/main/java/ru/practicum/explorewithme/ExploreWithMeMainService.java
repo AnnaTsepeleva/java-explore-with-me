5f4a0d664fc57f53288d95f4a520fa1c6c79bb4a
Anna <ann_0791@mail.ru>
2023-09-08 13:53:44 +0300

    fst commit

--- main/src/main/java/ru/practicum/explorewithme/ExploreWithMeMainService.java
+++ main/src/main/java/ru/practicum/explorewithme/ExploreWithMeMainService.java
@@ -3,7 +3,7 @@ package ru.practicum.explorewithme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.explorewithme"})
+@SpringBootApplication(scanBasePackages = {"ru.practicum"})
 public class ExploreWithMeMainService {
     public static void main(String[] args) {
 
