7997a065beb31b067e23f51c04ff78e522e83b6f
Anna <ann_0791@mail.ru>
2023-09-15 20:14:04 +0300

    fst commit

--- main/src/main/java/ru/practicum/explorewithme/controller/category/AdminCategoryController.java
+++ main/src/main/java/ru/practicum/explorewithme/controller/category/AdminCategoryController.java
@@ -1,7 +1,6 @@
 package ru.practicum.explorewithme.controller.category;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
