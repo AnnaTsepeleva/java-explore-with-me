18e1b044957fa2297b526e104117d00132ae2463
Anna <ann_0791@mail.ru>
2023-09-21 12:39:17 +0300

    fix: date validate in statistics server

--- main/src/main/java/ru/practicum/explorewithme/controller/category/AdminCategoryController.java
+++ main/src/main/java/ru/practicum/explorewithme/controller/category/AdminCategoryController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explorewithme.dtomain.category.CategoryDto;
-import ru.practicum.explorewithme.dtomain.category.NewCategoryDto;
 import ru.practicum.explorewithme.log.ToLog;
 import ru.practicum.explorewithme.mapper.CategoryMapper;
 import ru.practicum.explorewithme.model.Category;
--- main/src/main/java/ru/practicum/explorewithme/mapper/CategoryMapper.java
+++ main/src/main/java/ru/practicum/explorewithme/mapper/CategoryMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.explorewithme.mapper;
 
 import org.mapstruct.Mapper;
 import ru.practicum.explorewithme.dtomain.category.CategoryDto;
-import ru.practicum.explorewithme.dtomain.category.NewCategoryDto;
 import ru.practicum.explorewithme.model.Category;
 
 import javax.validation.Valid;
