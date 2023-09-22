4caec587cbc195215361a80b759cd8ba71aeea3f
Anna <ann_0791@mail.ru>
2023-09-22 21:32:57 +0300

    fix: date validate in statistics server

--- statistics/client/src/main/java/ru/practicum/explorewithme/stats/StatisticsClient.java
+++ statistics/client/src/main/java/ru/practicum/explorewithme/stats/StatisticsClient.java
@@ -1,6 +1,7 @@
 package ru.practicum.explorewithme.stats;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
--- statistics/client/src/main/java/ru/practicum/explorewithme/stats/StatsClientConfig.java
+++ statistics/client/src/main/java/ru/practicum/explorewithme/stats/StatsClientConfig.java
@@ -1,7 +1,13 @@
 package ru.practicum.explorewithme.stats;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class StatsClientConfig {
+    @Bean
+    public RestTemplate rest() {
+        return new RestTemplate();
+    }
 }
