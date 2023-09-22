c895a8066d404bf58888421cbc32a69cd77c59c3
Anna <ann_0791@mail.ru>
2023-09-22 21:34:43 +0300

    fix: date validate in statistics server

--- statistics/client/src/main/java/ru/practicum/explorewithme/stats/StatisticsClient.java
+++ statistics/client/src/main/java/ru/practicum/explorewithme/stats/StatisticsClient.java
@@ -1,7 +1,6 @@
 package ru.practicum.explorewithme.stats;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
